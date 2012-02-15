4e874a9fd62a9559171d1f9a4e471629444e9dd3
daveadams <daveadams@gmail.com>
2012-02-15 10:52:38 -0600

    Added testing stub and Rakefile

---
+++ Rakefile
@@ -0,0 +1,20 @@
+# Rakefile
+#   rake definitions
+#
+# Created 2012-02-15 daveadams@gmail.com
+# Last updated 2012-02-15 daveadams@gmail.com
+#
+# https://github.com/daveadams/sakai-info
+#
+# This software is public domain.
+#
+
+require 'rake/testtask'
+
+Rake::TestTask.new do |t|
+  t.libs << 'test'
+end
+
+desc "Run tests"
+task :default => :test
+
---
+++ test/test_simple.rb
@@ -0,0 +1,20 @@
+# test_simple.rb
+#   Simple testing stub
+#
+# Created 2012-02-15 daveadams@gmail.com
+# Last updated 2012-02-15 daveadams@gmail.com
+#
+# https://github.com/daveadams/sakai-info
+#
+# This software is public domain.
+#
+
+require 'test/unit'
+require 'sakai-info'
+
+class SimpleTest < Test::Unit::TestCase
+  def test_stub
+    assert_equal 1, 1
+  end
+end
+
