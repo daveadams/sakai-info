e3f59f0ecbd50ec5dd976dacc0dfea3beae0115b
daveadams <daveadams@gmail.com>
2012-02-16 05:20:16 -0600

    New class SakaiObject: base of other object classes, plus a first set of tests

---
+++ lib/sakai-info/sakai_object.rb
@@ -0,0 +1,50 @@
+# sakai_object.rb
+#   SakaiInfo::SakaiObject
+#
+# Created 2012-02-15 daveadams@gmail.com
+# Last updated 2012-02-15 daveadams@gmail.com
+#
+# https://github.com/daveadams/sakai-info
+#
+# This software is public domain.
+#
+
+module SakaiInfo
+  # this class forms the basis of all other Sakai object abstractions
+  class SakaiObject
+    def serialize(*q)
+      q.flatten!
+
+      if q.length == 0
+        q = [:default]
+      end
+
+      serialization = {}
+      q.each do |sub|
+        sub_method_name = (sub.to_s + "_serialization").to_sym
+        begin
+          sub_method = self.method(sub_method_name)
+          serialization = serialization.merge(sub_method.call)
+        rescue NameError
+          # ignore any missing serialization patterns
+        end
+      end
+
+      serialization
+    end
+
+    def default_serialization
+      {
+        "sakai_object_type" => self.class
+      }
+    end
+
+    def to_yaml
+      serialize.to_yaml
+    end
+
+    def to_json
+      serialize.to_json
+    end
+  end
+end
---
+++ test/test_sakai_object.rb
@@ -0,0 +1,73 @@
+# test_sakai_object.rb
+#   Tests for SakaiInfo::SakaiObject
+#
+# Created 2012-02-16 daveadams@gmail.com
+# Last updated 2012-02-16 daveadams@gmail.com
+#
+# https://github.com/daveadams/sakai-info
+#
+# This software is public domain.
+#
+
+require 'test/unit'
+require 'sakai-info'
+
+module SakaiInfo
+  class TestSakaiObjectSubclass < SakaiObject
+  end
+end
+
+class SakaiObjectTest < Test::Unit::TestCase
+  # test basic serialization methods
+  def test_serialization
+    so = SakaiInfo::SakaiObject.new
+
+    # specifying non-existent serializations should raise no NameErrors
+    assert_nothing_raised do
+      # many ways of asking for the same thing
+      # the default serialization should include the object classname
+      assert_equal({"sakai_object_type" => SakaiInfo::SakaiObject}, so.default_serialization)
+      assert_equal({"sakai_object_type" => SakaiInfo::SakaiObject}, so.serialize)
+      assert_equal({"sakai_object_type" => SakaiInfo::SakaiObject}, so.serialize(:default))
+      assert_equal({"sakai_object_type" => SakaiInfo::SakaiObject}, so.serialize("default"))
+
+      # requesting any other serializations without :default should result in empty hashes
+      # since no other serializations are defined at the base level
+      assert_equal({}, so.serialize(:summary))
+      assert_equal({}, so.serialize(:Default))
+      assert_equal({}, so.serialize("summary", "bakery"))
+      assert_equal({}, so.serialize(:non_existent_serialization, :and_another, :onemore))
+
+      # combining :default with anything else should bring us back to just
+      # the result for :default
+      assert_equal({"sakai_object_type" => SakaiInfo::SakaiObject},
+                   so.serialize(:default, :summary))
+      assert_equal({"sakai_object_type" => SakaiInfo::SakaiObject},
+                   so.serialize(:summary, :default))
+      assert_equal({"sakai_object_type" => SakaiInfo::SakaiObject},
+                   so.serialize("default", "summary"))
+      assert_equal({"sakai_object_type" => SakaiInfo::SakaiObject},
+                   so.serialize(:one, :two, :three, :default, :eight, :four, :two))
+
+      # asking for :default multiple times should produce only one copy of the data
+      assert_equal({"sakai_object_type" => SakaiInfo::SakaiObject},
+                   so.serialize(:default, :default))
+      assert_equal({"sakai_object_type" => SakaiInfo::SakaiObject},
+                   so.serialize(:default, :summary, :default))
+      assert_equal({"sakai_object_type" => SakaiInfo::SakaiObject},
+                   so.serialize(:summary, :default, :summary, :default, :summary))
+      assert_equal({"sakai_object_type" => SakaiInfo::SakaiObject},
+                   so.serialize(:default, :one, :default, :two, :default,
+                                :three, :default, :four, :default))
+    end
+  end
+
+  # TODO: test an extension class with multiple serializations
+  def test_subclass
+  end
+
+  # TODO: test yaml and json output
+  def test_serialization_strings
+  end
+end
+
