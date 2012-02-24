5e0efe54a4beb1991a93ebaf088a63eadab16f87
daveadams <daveadams@gmail.com>
2012-02-24 07:05:44 -0600

    Sequelized gradebook.rb

--- lib/sakai-info/gradebook.rb
+++ lib/sakai-info/gradebook.rb
@@ -2,7 +2,7 @@
 #   SakaiInfo::Gradebook library
 #
 # Created 2012-02-17 daveadams@gmail.com
-# Last updated 2012-02-18 daveadams@gmail.com
+# Last updated 2012-02-24 daveadams@gmail.com
 #
 # https://github.com/daveadams/sakai-info
 #
@@ -24,17 +24,16 @@ module SakaiInfo
     @@cache_by_site_id = {}
     def self.find(id)
       if @@cache[id].nil?
-        version = site = name = nil
-        DB.connect.exec("select version, gradebook_uid, name " +
-                        "from gb_gradebook_t " +
-                        "where id = :id", id) do |row|
-          version = row[0].to_i
-          site = Site.find(row[1])
-          name = row[2]
-        end
-        if version.nil?
+        row = DB.connect.fetch("select version, gradebook_uid, name " +
+                               "from gb_gradebook_t " +
+                               "where id = :id", id).first
+        if row.nil?
           raise ObjectNotFoundException.new(Gradebook, id)
         end
+
+        version = row[:version].to_i
+        site = Site.find(row[:gradebook_uid])
+        name = row[:name]
         @@cache[id] = Gradebook.new(id, version, site, name)
         @@cache_by_site_id[site.id] = @@cache[id]
       end
@@ -43,18 +42,17 @@ module SakaiInfo
 
     def self.find_by_site_id(site_id)
       if @@cache_by_site_id[site_id].nil?
-        id = version = site = name = nil
-        DB.connect.exec("select id, version, name " +
-                        "from gb_gradebook_t " +
-                        "where gradebook_uid = :site_id", site_id) do |row|
-          id = row[0].to_i
-          version = row[1].to_i
-          name = row[2]
-          site = Site.find(site_id)
-        end
-        if version.nil?
+        row = DB.connect.fetch("select id, version, name " +
+                               "from gb_gradebook_t " +
+                               "where gradebook_uid = ?", site_id).first
+        if row.nil?
           raise ObjectNotFoundException.new(Gradebook, site_id)
         end
+
+        id = row[:id].to_i
+        version = row[:version].to_i
+        name = row[:name]
+        site = Site.find(site_id)
         @@cache[id] = Gradebook.new(id, version, site, name)
         @@cache_by_site_id[site_id] = @@cache[id]
       end
@@ -89,24 +87,24 @@ module SakaiInfo
     @@cache = {}
     def self.find(id)
       if @@cache[id].nil?
-        gradebook = object_type = version = name = points_possible = due_date = weight = nil
-        DB.connect.exec("select gradebook_id, object_type_id, version, " +
-                        "name, points_possible, " +
-                        "to_char(due_date, 'YYYY-MM-DD'), " +
-                        "assignment_weighting " +
-                        "from gb_gradable_object_t " +
-                        "where id = :id", id) do |row|
-          gradebook = Gradebook.find(row[0].to_i)
-          object_type = row[1].to_i
-          version = row[2].to_i
-          name = row[3]
-          points_possible = row[4].to_f
-          due_date = row[5]
-          weight = row[6].to_f
-        end
-        if version.nil?
+        row = DB.connect.fetch("select gradebook_id, object_type_id, version, " +
+                               "name, points_possible, " +
+                               "to_char(due_date, 'YYYY-MM-DD') as due, " +
+                               "assignment_weighting " +
+                               "from gb_gradable_object_t " +
+                               "where id = ?", id).first
+
+        if row.nil?
           raise ObjectNotFoundException.new(GradableObject, id)
         end
+
+        gradebook = Gradebook.find(row[:gradebook_id].to_i)
+        object_type = row[:object_type_id].to_i
+        version = row[:version].to_i
+        name = row[:name]
+        points_possible = row[:points_possible].to_f
+        due_date = row[:due]
+        weight = row[:assignment_weighting].to_f
         @@cache[id] = GradableObject.new(id, gradebook, object_type,
                                          version, name, points_possible,
                                          due_date, weight)
@@ -119,20 +117,20 @@ module SakaiInfo
       if @@cache_by_gradebook_id[gradebook_id].nil?
         objects = []
         gradebook = Gradebook.find(gradebook_id)
-        DB.connect.exec("select id, object_type_id, version, " +
-                        "name, points_possible, " +
-                        "to_char(due_date, 'YYYY-MM-DD'), " +
-                        "assignment_weighting " +
-                        "from gb_gradable_object_t " +
-                        "where gradebook_id = :gradebook_id " +
-                        "order by due_date asc", gradebook_id) do |row|
-          id = row[0].to_i
-          object_type = row[1].to_i
-          version = row[2].to_i
-          name = row[3]
-          points_possible = row[4].to_f
-          due_date = row[5]
-          weight = row[6].to_f
+        DB.connect.fetch("select id, object_type_id, version, " +
+                         "name, points_possible, " +
+                         "to_char(due_date, 'YYYY-MM-DD') as due, " +
+                         "assignment_weighting " +
+                         "from gb_gradable_object_t " +
+                         "where gradebook_id = ? " +
+                         "order by due_date asc", gradebook_id) do |row|
+          id = row[:id].to_i
+          object_type = row[:object_type_id].to_i
+          version = row[:version].to_i
+          name = row[:name]
+          points_possible = row[:points_possible].to_f
+          due_date = row[:due]
+          weight = row[:assignment_weighting].to_f
           objects << GradableObject.new(id, gradebook, object_type,
                                         version, name, points_possible,
                                         due_date, weight)
