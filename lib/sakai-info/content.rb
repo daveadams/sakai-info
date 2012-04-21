9e20e651c0fa6d5a4131b3cf0ab36364ea409f43
daveadams <daveadams@gmail.com>
2012-04-21 10:33:34 -0500

    content.rb: added binary entity parsing for resources, fixed database access, moved to dbrow-style constructor, cleaned up serializations

--- lib/sakai-info/content.rb
+++ lib/sakai-info/content.rb
@@ -2,7 +2,7 @@
 #   SakaiInfo::Content library
 #
 # Created 2012-02-17 daveadams@gmail.com
-# Last updated 2012-04-02 daveadams@gmail.com
+# Last updated 2012-04-21 daveadams@gmail.com
 #
 # https://github.com/daveadams/sakai-info
 #
@@ -37,16 +37,12 @@ module SakaiInfo
       end
     end
 
-    def binary
-      if @binary.nil?
-        row = DB.connect[@table_name.to_sym].filter(@id_column.to_sym => @id).first
-        @binary = row[:binary_entity].read
-      end
-      @binary
-    end
-
     def size_on_disk
       0
+    end
+
+    def binary_entity
+      @binary_entity ||= ContentBinaryEntity.new(@dbrow[:binary_entity])
     end
 
     def default_serialization
@@ -71,7 +67,7 @@ module SakaiInfo
       else
         begin
           @realm ||= AuthzRealm.find_by_name("/content#{@id}")
-        rescue AuthzRealmNotFoundException
+        rescue ObjectNotFoundException
           @realm_is_nil = true
           nil
         end
@@ -81,19 +77,53 @@ module SakaiInfo
     def effective_realm
       self.realm || (parent.nil? ? nil : parent.effective_realm)
     end
+
+    def realm_serialization
+      if self.effective_realm.nil?
+        {}
+      else
+        {
+          "effective_realm" => self.effective_realm.name,
+        }
+      end
+    end
+
+    def child_summary_serialization
+      {
+        "id" => self.id.gsub(/^#{self.parent_id}/,""),
+        "size" => self.size_on_disk,
+      }
+    end
+
+    def properties_serialization
+      result = self.binary_entity.to_hash
+      if result["groups"] == []
+        result.delete("groups")
+      end
+      result.keys.each do |key|
+        if result[key].nil? or result[key] == ""
+          result.delete(key)
+        end
+      end
+      {
+        "properties" => result
+      }
+    end
   end
 
   class ContentResource < Content
-    attr_reader :file_path, :uuid, :context, :resource_type_id
+    attr_reader :file_path, :uuid, :context, :resource_type_id, :dbrow
 
-    def initialize(id, parent_id, file_path, uuid, file_size, context, resource_type_id)
-      @id = id
-      @parent_id = parent_id
-      @file_path = File.join(Instance.content_base_directory, file_path)
-      @uuid = uuid
-      @file_size = file_size
-      @context = context
-      @resource_type_id = resource_type_id
+    def initialize(dbrow)
+      @dbrow = dbrow
+
+      @id = @dbrow[:resource_id]
+      @parent_id = @dbrow[:in_collection]
+      @file_path = @dbrow[:file_path]
+      @uuid = @dbrow[:resource_uuid]
+      @file_size = @dbrow[:file_size].to_i
+      @context = @dbrow[:context]
+      @resource_type_id = @dbrow[:resource_type_id]
 
       @table_name = "content_resource"
       @id_column = "resource_id"
@@ -102,11 +132,11 @@ module SakaiInfo
     @@cache = {}
     def self.find(id)
       if @@cache[id].nil?
-        row = DB.connect[:content_resource].filter(:resource_id => id).first
+        row = DB.connect[:content_resource].where(:resource_id => id).first
         if row.nil?
           raise ObjectNotFoundException.new(ContentResource, id)
         end
-        @@cache[id] = ContentResource.new(row[:resource_id], row[:in_collection], row[:file_path], row[:resource_uuid], row[:file_size].to_i, row[:context], row[:resource_type_id])
+        @@cache[id] = ContentResource.new(row)
       end
       @@cache[id]
     end
@@ -115,40 +145,54 @@ module SakaiInfo
       @file_size
     end
 
-    def default_serialization
-      {
-        "id" => self.id,
-        "parent" => self.parent_id,
-        "uuid" => self.uuid,
-        "file_path" => self.file_path,
-        "size_on_disk" => self.size_on_disk,
-        "context" => self.context,
-        "resource_type_id" => self.resource_type_id,
-        "effective_realm" => (self.effective_realm.nil? ? nil : self.effective_realm.name)
-      }
+    def self.query_by_parent(parent_id)
+      DB.connect[:content_resource].where(:in_collection => parent_id)
     end
 
     def self.find_by_parent(parent_id)
       resources = []
-      DB.connect[:content_resource].filter(:in_collection => parent_id) do |row|
-        @@cache[row[:resource_id]] =
-          ContentResource.new(row[:resource_id], row[:in_collection], row[:file_path],
-                                row[:resource_uuid], row[:file_size].to_i, row[:context],
-                                row[:resource_type_id])
-        resources << @@cache[row[0]]
+      ContentResource.query_by_parent(parent_id).all.each do |row|
+        @@cache[row[:resource_id]] = ContentResource.new(row)
+        resources << @@cache[row[:resource_id]]
       end
       resources
     end
 
     def self.count_by_parent(parent_id)
-      DB.connect[:content_resource].filter(:in_collection => parent_id).count
+      ContentResource.query_by_parent(parent_id).count
+    end
+
+    def default_serialization
+      result = {
+        "id" => self.id,
+        "uuid" => self.uuid,
+        "file_path" => self.file_path,
+        "size_on_disk" => self.size_on_disk,
+        "context" => self.context,
+        "resource_type" => self.resource_type_id,
+      }
+      if result["uuid"].nil?
+        result.delete("uuid")
+      end
+      result
+    end
+
+    def summary_serialization
+      {
+        "id" => self.id,
+        "size" => self.size_on_disk,
+      }
     end
   end
 
   class ContentCollection < Content
-    def initialize(id, parent_id)
-      @id = id
-      @parent_id = parent_id
+    attr_reader :dbrow
+
+    def initialize(dbrow)
+      @dbrow = dbrow
+
+      @id = @dbrow[:collection_id]
+      @parent_id = @dbrow[:in_collection]
 
       @table_name = "content_collection"
       @id_column = "collection_id"
@@ -161,11 +205,11 @@ module SakaiInfo
       end
 
       if @@cache[id].nil?
-        row = DB.connect[:content_collection].filter(:collection_id => id).first
+        row = DB.connect[:content_collection].where(:collection_id => id).first
         if row.nil?
           raise ObjectNotFoundException.new(ContentCollection, id)
         end
-        @@cache[id] = ContentCollection.new(row[:collection_id], row[:in_collection])
+        @@cache[id] = ContentCollection.new(row)
       end
       @@cache[id]
     end
@@ -183,15 +227,10 @@ module SakaiInfo
       collections = []
       DB.connect[:content_collection].
         where(:collection_id.like('%/portfolio-interaction/')) do |row|
-        @@cache[row[:collection_id]] =
-          ContentCollection.new(row[:collection_id], row[:in_collection])
+        @@cache[row[:collection_id]] = ContentCollection.new(row)
         collections << @@cache[row[:collection_id]]
       end
       collections
-    end
-
-    def self.count_by_parent(parent_id)
-      DB.connect[:content_collection].filter(:in_collection => parent_id).count
     end
 
     def size_on_disk
@@ -230,36 +269,47 @@ module SakaiInfo
     def default_serialization
       {
         "id" => self.id,
-        "parent" => self.parent_id,
         "size_on_disk" => self.size_on_disk,
         "children" => self.child_counts,
-        "effective_realm" => (self.effective_realm.nil? ? nil : effective_realm.name)
       }
     end
 
     def summary_serialization
       {
         "id" => self.id,
-        "parent" => self.parent_id
       }
     end
 
     def children_serialization
-      {
+      result = {
         "collections" => self.children["collections"].collect { |cc|
-          cc.serialize(:summary, :children)
+          cc.serialize(:child_summary, :children)
         },
         "resources" => self.children["resources"].collect { |cr|
-          cr.serialize(:summary)
+          cr.serialize(:child_summary)
         }
       }
+      if result["collections"] == []
+        result.delete("collections")
+      end
+      if result["resources"] == []
+        result.delete("resources")
+      end
+      result
+    end
+
+    def self.query_by_parent(parent_id)
+      DB.connect[:content_collection].where(:in_collection => parent_id)
+    end
+
+    def self.count_by_parent(parent_id)
+      ContentCollection.query_by_parent(parent_id).count
     end
 
     def self.find_by_parent(parent_id)
       collections = []
-      DB.connect[:content_collection].filter(:in_collection => parent_id) do |row|
-        @@cache[row[:collection_id]] =
-          ContentCollection.new(row[:collection_id], row[:in_collection])
+      ContentCollection.query_by_parent(parent_id).all.each do |row|
+        @@cache[row[:collection_id]] = ContentCollection.new(row)
         collections << @@cache[row[:collection_id]]
       end
       collections
@@ -277,208 +327,175 @@ module SakaiInfo
     end
   end
 
-  # TODO: merge binary_entity parsing code into content
+  class UnknownBinaryEntityFormat < SakaiException; end
+  class ContentBinaryEntity
+    require 'stringio'
 
-  # require 'stringio'
+    def read_tiny_int(bin)
+      bin.read(1).unpack("C")[0]
+    end
 
-  # def read_tiny_int(blob)
-  #   blob.read(1).unpack("C")[0]
-  # end
+    def read_int(bin)
+      bin.read(2).unpack("n")[0]
+    end
 
-  # def read_int(blob)
-  #   blob.read(2).unpack("n")[0]
-  # end
+    def read_long_int(bin)
+      bin.read(4).unpack("N")[0]
+    end
 
-  # def read_long_int(blob)
-  #   blob.read(4).unpack("N")[0]
-  # end
+    def read_huge_int(bin)
+      part1 = bin.read(4).unpack("N")[0]
+      part2 = bin.read(4).unpack("N")[0]
 
-  # def read_huge_int(blob)
-  #   part1 = blob.read(4).unpack("N")[0]
-  #   part2 = blob.read(4).unpack("N")[0]
+      return ((part1 << 32) + part2)
+    end
+    MAX_HUGE_INT=(2**64 - 1)
 
-  #   return ((part1 << 32) + part2)
-  # end
-  # MAX_HUGE_INT=(2**64 - 1)
+    def read_utf_string(bin)
+      byte_length = read_int(bin)
+      if byte_length == 0
+        return ""
+      end
+      bin.read(byte_length).force_encoding(Encoding::UTF_8)
+    end
 
-  # def read_utf_string(blob)
-  #   # 'n' means big-endian 16-bit unsigned int
-  #   byte_length = read_int(blob)
-  #   if byte_length == 0
-  #     return ""
-  #   end
-  #   blob.read(byte_length).force_encoding(Encoding::UTF_8)
-  # end
+    def initialize(raw_blob)
+      blob = StringIO.new(raw_blob)
+      blob_id = blob.read(6)
 
-  # ENV["NLS_LANG"] ||= "AMERICAN_AMERICA.UTF8"
+      serialization_type = read_long_int(blob)
+      if serialization_type != 1
+        raise UnknownBinaryEntityFormat.new("Unrecognized format version #{serialization_type}")
+      end
 
-  # require 'oci8'
-  # db = OCI8.new("sakai", "ootfpitf", "SAKAI")
+      @properties = {}
 
-  # resource_id = ARGV[0]
-  # blob = nil
-  # db.exec("select binary_entity from content_resource where resource_id=:id", resource_id) do |row|
-  #   blob = StringIO.new(row[0].read)
-  # end
+      if blob_id == "CHSBRE"
+        parse_for_content_resource(blob)
+      elsif blob_id == "CHSBCE"
+        parse_for_content_collection(blob)
+      else
+        raise UnknownBinaryEntityFormat.new("Unrecognized format ID: #{blob_id}")
+      end
+    end
 
-  # if blob.nil?
-  #   puts "No such resource"
-  #   exit 1
-  # end
+    def [](key)
+      @properties[key]
+    end
 
-  # puts "Blob size: #{blob.size}"
+    def keys
+      @properties.keys
+    end
 
-  # blob_id = blob.read(6)
-  # puts "Blob ID: #{blob_id}"
+    def to_hash
+      @properties
+    end
 
-  # serialization_type = read_long_int(blob)
-  # puts "Serialization type: #{serialization_type}"
+    # content block IDs
+    # don't ask me why the blocks are coded this way
+    # BLOCK1 = general attributes
+    CR_BLOCK1 = 10
+    # BLOCK2 = release and retract dates
+    CR_BLOCK2 = 12
+    # BLOCK3 = groups
+    CR_BLOCK3 = 11
+    # BLOCK4 = properties
+    CR_BLOCK4 = 13
+    # BLOCK5 = file properties
+    CR_BLOCK5 = 14
+    # BLOCK6 = byte[] storing file content if it's not in the filesystem
+    CR_BLOCK6 = 15
+    # BLOCK_END
+    CR_BLOCK_END = 2
 
-  # # don't ask me why the blocks are coded this way
-  # # BLOCK1 = general attributes
-  # BLOCK1 = 10
-  # # BLOCK2 = release and retract dates
-  # BLOCK2 = 12
-  # # BLOCK3 = groups
-  # BLOCK3 = 11
-  # # BLOCK4 = properties
-  # BLOCK4 = 13
-  # # BLOCK5 = file properties
-  # BLOCK5 = 14
-  # # BLOCK6 = byte[] storing file content if it's not in the filesystem
-  # BLOCK6 = 15
-  # # BLOCK_END
-  # BLOCK_END = 2
+    # properties block types
+    CR_PROPS_BLOCK1 = 100
+    CR_PROPS_BLOCK2 = 101
+    CR_PROPS_BLOCK3 = 102
 
-  # # properties block types
-  # PROPS_BLOCK1 = 100
-  # PROPS_BLOCK2 = 101
-  # PROPS_BLOCK3 = 102
+    def parse_for_content_resource(blob)
+      while true
+        block_number = read_long_int(blob)
+        case(block_number)
+        when CR_BLOCK1
+          @properties["id"] = read_utf_string(blob)
+          @properties["resource_type"] = read_utf_string(blob)
+          @properties["access_mode"] = read_utf_string(blob)
+          @properties["is_hidden"] = (read_tiny_int(blob) != 0)
 
-  # while true
-  #   puts
-  #   puts "Starting new block"
-  #   puts "Current position: #{blob.pos}"
+        when CR_BLOCK2
+          @properties["release_date"] = read_huge_int(blob)
+          if @properties["release_date"] == MAX_HUGE_INT
+            @properties["release_date"] = nil
+          end
 
-  #   block_number = read_long_int(blob)
-  #   case(block_number)
-  #   when BLOCK1
-  #     puts "BLOCK1"
+          @properties["retract_date"] = read_huge_int(blob)
+          if @properties["retract_date"] == MAX_HUGE_INT
+            @properties["retract_date"] = nil
+          end
 
-  #     # read ID
-  #     id = read_utf_string(blob)
-  #     puts "Content ID: #{id}"
+        when CR_BLOCK3
+          group_count = read_long_int(blob)
+          @properties["groups"] = []
+          if group_count > 0
+            group_count.times do
+              @properties["groups"] << read_utf_string(blob)
+            end
+          end
 
-  #     # read resource type
-  #     resource_type = read_utf_string(blob)
-  #     puts "Resource Type: #{resource_type}"
+        when CR_BLOCK4
+          props_serialization_type = read_long_int(blob)
+          if props_serialization_type != 1
+            raise UnknownBinaryEntityFormat.new("Unrecognized properties serialization type #{properties_serialization_type}")
+          end
 
-  #     access_mode = read_utf_string(blob)
-  #     puts "Access Mode: #{access_mode}"
+          props_block_number = read_long_int(blob)
+          if props_block_number == CR_PROPS_BLOCK1
+            props_count = read_long_int(blob)
+          else
+            raise UnknownBinaryEntityFormat.new("Unable to parse properties block")
+          end
 
-  #     is_hidden = (read_tiny_int(blob) != 0)
-  #     puts "Hidden?: #{is_hidden}"
+          props_count.times do
+            props_block_number = read_long_int(blob)
+            case props_block_number
+            when CR_PROPS_BLOCK2
+              key = read_utf_string(blob)
+              value = read_utf_string(blob)
+              @properties[key] = value
+            when CR_PROPS_BLOCK3
+              key = read_utf_string(blob)
+              @properties[key] = []
+              value_count = read_long_int(blob)
+              value_count.times do
+                @properties[key] << read_utf_string(blob)
+              end
+            else
+              raise UnknownBinaryEntityFormat.new("Unknown Property Block ID: '#{block_number}'")
+            end
+          end
 
-  #   when BLOCK2
-  #     puts "BLOCK2"
+        when CR_BLOCK5
+          @properties["content_type"] = read_utf_string(blob)
+          @properties["content_length"] = read_huge_int(blob)
+          @properties["file_path"] = read_utf_string(blob)
 
-  #     releaseDate = read_huge_int(blob)
-  #     if releaseDate == MAX_HUGE_INT
-  #       puts "Release Date: n/a"
-  #     else
-  #       puts "Release Date: #{releaseDate}"
-  #     end
+        when CR_BLOCK6
+          body_size = read_long_int(blob)
+          if body_size > 0
+            STDERR.puts "WARNING: files stored in the database are not currently supported"
+          end
 
-  #     retractDate = read_huge_int(blob)
-  #     if retractDate == MAX_HUGE_INT
-  #       puts "Retract Date: n/a"
-  #     else
-  #       puts "Retract Date: #{retractDate}"
-  #     end
+        when CR_BLOCK_END
+          break
 
-  #   when BLOCK3
-  #     puts "BLOCK3"
+        else
+          raise UnknownBinaryEntityFormat.new("Unknown Block ID: '#{block_number}'")
+        end
+      end
+    end
 
-  #     group_count = read_long_int(blob)
-  #     puts "Group count: #{group_count}"
-  #     if group_count > 0
-  #       puts "Groups:"
-  #       groups = []
-  #       group_count.times do
-  #         groups << read_utf_string(blob)
-  #       end
-  #       groups.each do |g|
-  #         puts "  - #{g}"
-  #       end
-  #     end
-
-  #   when BLOCK4
-  #     puts "BLOCK4"
-
-  #     # properties
-  #     props_serialization_type = read_long_int(blob)
-  #     puts "Properties Serialization Type: #{props_serialization_type}"
-
-  #     props_block_number = read_long_int(blob)
-  #     if props_block_number == PROPS_BLOCK1
-  #       #puts "PROPS_BLOCK1"
-  #       props_count = read_long_int(blob)
-  #       puts "Property Count: #{props_count}"
-  #     else
-  #       puts "Failed to parse Property Block"
-  #       exit 1
-  #     end
-
-  #     props_count.times do
-  #       props_block_number = read_long_int(blob)
-  #       case props_block_number
-  #       when PROPS_BLOCK2
-  #         #puts "PROPS_BLOCK2 -- key-value pair"
-  #         key = read_utf_string(blob)
-  #         value = read_utf_string(blob)
-  #         puts "#{key}: #{value}"
-  #       when PROPS_BLOCK3
-  #         #puts "PROPS_BLOCK3 -- key-list pair"
-  #         key = read_utf_string(blob)
-  #         puts "#{key}:"
-  #         value_count = read_long_int(blob)
-  #         value_count.times do
-  #           value = read_utf_string(blob)
-  #           puts "  - #{value}"
-  #         end
-  #       else
-  #         puts "PROPS_UNKNOWN_BLOCK"
-  #         exit 1
-  #       end
-  #     end
-
-  #   when BLOCK5
-  #     puts "BLOCK5"
-
-  #     content_type = read_utf_string(blob)
-  #     puts "Content Type: #{content_type}"
-
-  #     content_length = read_huge_int(blob)
-  #     puts "Content Length: #{content_length}"
-
-  #     file_path = read_utf_string(blob)
-  #     puts "File Path: #{file_path}"
-
-  #   when BLOCK6
-  #     puts "BLOCK6"
-  #     body_size = read_long_int(blob)
-  #     if body_size > 0
-  #       puts "Body exists in DB record!"
-  #       exit 1
-  #     end
-
-  #   when BLOCK_END
-  #     puts "BLOCK_END: #{blob.pos}"
-  #     exit
-  #   else
-  #     puts "UNKNOWN BLOCK: '#{block_number}'!"
-  #     exit
-  #   end
-  # end
-
+    def parse_for_content_collection(blob)
+    end
+  end
 end
