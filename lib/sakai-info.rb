abe99fe4486ddb324f5ed569c18d3cb0db114224
daveadams <daveadams@gmail.com>
2012-04-22 08:21:05 -0500

    sakai-info.rb: HACK: terrible hack to fix case sensitivity in mysql results

--- lib/sakai-info.rb
+++ lib/sakai-info.rb
@@ -2,7 +2,7 @@
 #   Base library file
 #
 # Created 2012-02-15 daveadams@gmail.com
-# Last updated 2012-04-21 daveadams@gmail.com
+# Last updated 2012-04-22 daveadams@gmail.com
 #
 # https://github.com/daveadams/sakai-info
 #
@@ -71,10 +71,33 @@ module SakaiInfo
   end
 end
 
+######################################################################
 # extensions to other objects
 class String
   def is_uuid?
     self =~ /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
+  end
+end
+
+# terrible hack to work around mysql case issues
+# essentially, if the hash key asked for is a symbol and its value is
+# nil, then try again with the uppercased version of the symbol
+# this might cause problems in weird cases with other hashes, this is
+# definitely not a sustainable fix.
+# TODO: patch Sequel for case-insensitive/-fixed identifiers
+class Hash
+  alias :original_brackets :[]
+
+  def [](key)
+    if not (value = original_brackets(key)).nil?
+      return value
+    else
+      if key.is_a? Symbol
+        return original_brackets(key.to_s.upcase.to_sym)
+      else
+        return nil
+      end
+    end
   end
 end
 
