68d7443c7e024c1db09fc7649a37a70a85f2c707
daveadams <daveadams@gmail.com>
2012-06-21 09:39:07 -0500

    cli: moved lookup modes over to lookup.rb

--- lib/sakai-info/cli.rb
+++ lib/sakai-info/cli.rb
@@ -2,7 +2,7 @@
 #  - sakai-info command line tool support
 #
 # Created 2012-02-19 daveadams@gmail.com
-# Last updated 2012-05-23 daveadams@gmail.com
+# Last updated 2012-06-21 daveadams@gmail.com
 #
 # https://github.com/daveadams/sakai-info
 #
@@ -12,40 +12,8 @@
 require 'sakai-info/cli/help'
 require 'sakai-info/cli/lookup'
 require 'sakai-info/cli/query'
+require 'sakai-info/cli/special'
 
 # it's faster to run single-threaded
 Sequel.single_threaded = true
-
-module SakaiInfo
-  module CLI
-    ObjectModes = {
-      "site" => Site,
-      "page" => Page,
-      "tool" => Tool,
-      "user" => User,
-      "group" => Group,
-      "quiz" => Quiz,
-      "quiz-section" => QuizSection,
-      "quiz-item" => QuizItem,
-      "quiz-attempt" => QuizAttempt,
-      "quiz-attempt-item" => QuizAttemptItem,
-      "quiz-attempt-item-attachment" => QuizAttemptItemAttachment,
-      "qpool" => QuestionPool,
-      "question-pool" => QuestionPool,
-      "assignment" => Assignment,
-      "assignment-submission" => AssignmentSubmission,
-      "forum" => Forum,
-      "forum-thread" => ForumThread,
-      "forum-post" => ForumPost,
-      "content" => Content,
-      "announcement" => Announcement,
-      "announcement-channel" => AnnouncementChannel,
-      "gradebook" => Gradebook,
-      "gradebook-item" => GradebookItem,
-      "role" => AuthzRole,
-      "function" => AuthzFunction,
-      "realm" => AuthzRealm,
-    }
-  end
-end
 
--- lib/sakai-info/cli/lookup.rb
+++ lib/sakai-info/cli/lookup.rb
@@ -2,7 +2,7 @@
 #   class for handling the default command line mode
 #
 # Created 2012-05-23 daveadams@gmail.com
-# Last updated 2012-05-23 daveadams@gmail.com
+# Last updated 2012-06-21 daveadams@gmail.com
 #
 # https://github.com/daveadams/sakai-info
 #
@@ -17,7 +17,7 @@ module SakaiInfo
         id = args.shift
 
         if flags.include? "--all"
-          serials = CLI::ObjectModes[object_type].all_serializations
+          serials = CLI::LookupModes[object_type].all_serializations
         elsif
           flags.include? "--dbrow-only"
           serials = [:dbrow_only]
@@ -25,13 +25,72 @@ module SakaiInfo
           serials = [:default] + flags.collect{|flag|flag.gsub(/^--/,'').gsub("-","_").to_sym}
         end
         begin
-          puts CLI::ObjectModes[object_type].find(id).to_yaml(serials)
+          puts CLI::LookupModes[object_type].find(id).to_yaml(serials)
         rescue ObjectNotFoundException
           STDERR.puts "ERROR: Could not find #{object_type} with an ID of '#{id}'"
           exit 1
         end
       end
     end
+
+    LookupModes = {
+      "site" => Site,
+      "page" => Page,
+      "tool" => Tool,
+      "user" => User,
+      "group" => Group,
+      "pq" => PublishedQuiz,
+      "pubquiz" => PublishedQuiz,
+      "published-quiz" => PublishedQuiz,
+      "nq" => PendingQuiz,
+      "penquiz" => PendingQuiz,
+      "pending-quiz" => PendingQuiz,
+      "pqs" => PublishedQuizSection,
+      "pubqs" => PublishedQuizSection,
+      "published-quiz-section" => PublishedQuizSection,
+      "nqs" => PendingQuizSection,
+      "penqs" => PendingQuizSection,
+      "pending-quiz-section" => PendingQuizSection,
+      "pqi" => PublishedQuizItem,
+      "pubqi" => PublishedQuizItem,
+      "published-quiz-item" => PublishedQuizItem,
+      "nqi" => PendingQuizItem,
+      "penqi" => PendingQuizItem,
+      "pending-quiz-item" => PendingQuizItem,
+      "qa" => QuizAttempt,
+      "quiz-attempt" => QuizAttempt,
+      "qai" => QuizAttemptItem,
+      "quiz-attempt-item" => QuizAttemptItem,
+      "qaia" => QuizAttemptItemAttachment,
+      "quiz-attempt-item-attachment" => QuizAttemptItemAttachment,
+      "qp" => QuestionPool,
+      "qpool" => QuestionPool,
+      "question-pool" => QuestionPool,
+      "a" => Assignment,
+      "assignment" => Assignment,
+      "as" => AssignmentSubmission,
+      "asub" => AssignmentSubmission,
+      "assignment-submission" => AssignmentSubmission,
+      "forum" => Forum,
+      "ft" => ForumThread,
+      "forum-thread" => ForumThread,
+      "fp" => ForumPost,
+      "forum-post" => ForumPost,
+      "content" => Content,
+      "ann" => Announcement,
+      "announcement" => Announcement,
+      "annchan" => AnnouncementChannel,
+      "announcement-channel" => AnnouncementChannel,
+      "gb" => Gradebook,
+      "gradebook" => Gradebook,
+      "gbi" => GradebookItem,
+      "gradebook-item" => GradebookItem,
+      "role" => AuthzRole,
+      "fn" => AuthzFunction,
+      "func" => AuthzFunction,
+      "function" => AuthzFunction,
+      "realm" => AuthzRealm,
+    }
   end
 end
 
