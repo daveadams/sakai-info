dde5ffd638afb87d59acb611e51a469ccb8214e4
daveadams <daveadams@gmail.com>
2012-10-12 14:41:07 -0500

    Better test helper

--- test/testlib/helper.rb
+++ test/testlib/helper.rb
@@ -11,3 +11,18 @@
 
 require 'minitest/autorun'
 require 'sakai-info'
+include SakaiInfo
+
+class Helper
+  TestDatabaseFile = File.expand_path("../../tmp/db/test.db", File.dirname(__FILE__))
+
+  def self.test_database_available?
+    if File.exist? TestDatabaseFile
+      DB.configure({:test => "sqlite://#{TestDatabaseFile}"})
+      true
+    else
+      false
+    end
+  end
+end
+
