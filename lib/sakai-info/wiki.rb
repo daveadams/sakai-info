bb4c626df93554086afaaf96050f8c5d719b5681
daveadams <daveadams@gmail.com>
2012-10-25 06:57:49 -0500

    wiki.rb: filling out remote properties

--- lib/sakai-info/wiki.rb
+++ lib/sakai-info/wiki.rb
@@ -2,7 +2,7 @@
 #   SakaiInfo::Wiki library
 #
 # Created 2012-10-23 daveadams@gmail.com
-# Last updated 2012-10-23 daveadams@gmail.com
+# Last updated 2012-10-25 daveadams@gmail.com
 #
 # https://github.com/daveadams/sakai-info
 #
@@ -36,19 +36,57 @@ module SakaiInfo
     end
 
     def owner
-      @owner ||= User.find(@dbrow[:owner])
+      @owner ||= User.find!(@dbrow[:owner])
     end
 
-    def name
-      @dbrow[:name]
+    def realm
+      if not self.realm_name.nil?
+        @realm ||= AuthzRealm.find!(self.realm_name)
+      end
+    end
+
+    def realm_name
+      @dbrow[:realm]
+    end
+
+    def site_id
+      @site_id ||= if not self.realm_name.nil?
+                     if self.realm_name =~ /^\/site\/([^\/]+)/
+                       $1
+                     else
+                       nil
+                     end
+                   else
+                     nil
+                   end
+    end
+
+    def site
+      @site ||= if not self.site_id.nil?
+                  Site.find!(self.site_id)
+                else
+                  nil
+                end
+    end
+
+    def page_name
+      @page_name ||= if not self.realm_name.nil?
+                       if @dbrow[:name] =~ /^#{self.realm_name}\/(.+)$/
+                         $1
+                       else
+                         @dbrow[:name]
+                       end
+                     end
     end
 
     # serialization
     def default_serialization
       result = {
         "id" => self.id,
-        "name" => self.name,
+        "page_name" => self.page_name,
+        "realm" => self.realm.serialize(:summary),
         "owner" => self.owner.serialize(:summary),
+        "site" => self.site.serialize(:summary),
       }
       result
     end
@@ -58,6 +96,7 @@ module SakaiInfo
         "id" => self.id,
         "name" => self.name,
         "owner" => self.owner.eid,
+        "site_id" => self.site_id,
       }
     end
 
